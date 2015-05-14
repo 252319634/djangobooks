c735fccde30d0567f3cdd175fdcab1293b1cdede
xp <252319634.qq.com>
2015-05-14 20:22:30 +0800

    提交所有的项目文件

--- books/views.py
+++ books/views.py
@@ -1,9 +1,47 @@
+from django.http import HttpResponse, Http404
 from django.shortcuts import render
-
-# Create your views here.
 from django.shortcuts import render_to_response
+from django.template import Context
+from django.template.loader import get_template
 from models import Book
+import datetime
+
+
+def hours_ahead(request, offset):
+    try:
+        offset = int(offset)
+    except ValueError:
+        raise Http404()
+    dt = datetime.datetime.now() + datetime.timedelta(hours=offset)
+    assert False
+    html = "<html><body>In %s hour(s), it will be %s.</body></html>" % (offset, dt)
+    return HttpResponse(html)
+
 
 def latest_books(request):
     book_list = Book.objects.order_by('-pub_date')[:10]
     return render_to_response('latest_books.html', {'book_list': book_list})
+
+
+# def current_datetime(request):
+# now = datetime.datetime.now()
+#     html = "<html><body>It is now %s.</body></html>" % now
+#     return HttpResponse(html)
+# def current_datetime(request):
+#     now = datetime.datetime.now()
+#     t = get_template('current_datetime.html')
+#     html = t.render(Context({'current_date': now}))
+#     return HttpResponse(html)
+def current_datetime(request):
+    now = datetime.datetime.now()
+    return render_to_response('current_datetime.html', {'current_date': now})
+
+
+def current_section(request):
+    title = 'mypage'
+    current_section = 'mypage->nav.html'
+    return render_to_response('mypage.html', locals())
+
+
+def hello(request):
+    return HttpResponse("Hello world")
--- djangoBook/urls.py
+++ djangoBook/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-from django.conf.urls.defaults import *
+# from django.conf.urls.defaults import *
 from books import views
+
+
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'djangoBook.views.home', name='home'),
@@ -9,4 +11,8 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^latest/$', views.latest_books),
+    url(r'^time/$', views.current_datetime),
+    url(r'^hello/$', views.hello),
+    url(r'^time/plus/(\d{1,2})/$', views.hours_ahead),
+    url(r'^mypage/$', views.current_section),
 )
