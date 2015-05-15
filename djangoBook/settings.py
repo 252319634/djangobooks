c034e35f01cf56aaa0265cd836599d7cf53844ed
xp <252319634.qq.com>
2015-05-15 14:50:56 +0800

    提交所有的项目文件

--- djangoBook/settings.py
+++ djangoBook/settings.py
@@ -59,22 +59,22 @@ WSGI_APPLICATION = 'djangoBook.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'mydb',
-        'USER': 'root',
-        'PASSWORD': '123456',
-        'HOST': '127.0.0.1',
-    }
-}
-
 # DATABASES = {
 # 'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'mydb',
+#         'USER': 'root',
+#         'PASSWORD': '123456',
+#         'HOST': '127.0.0.1',
 #     }
 # }
+
+DATABASES = {
+'default': {
+    'ENGINE': 'django.db.backends.sqlite3',
+    'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+}
+}
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
